1643fc753095346a68e5efbcd1bab49a51113bce
rajmanohar04 <rajmanohar.mone04@gmail.com>
2024-11-09 09:29:37 +0530

    Update MenuLocators.java
    Updated few Locators

--- src/main/java/WidgetsLocators/MenuLocators.java
+++ src/main/java/WidgetsLocators/MenuLocators.java
@@ -1,14 +1,24 @@
 package WidgetsLocators;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import Resources.Base;
 
-public class MenuLocators extends Base{
-	
-	@FindBy(xpath="//ul[@id='nav']/li[2]/ul/li[3]/ul/li[1]/a")
+public class MenuLocators extends Base {
+
+	@FindBy(xpath = "//ul[@id='nav']/li[2]/ul/li[3]/ul/li[1]/a")
 	WebElement SubSubItem1;
 
-	@FindBy(xpath="//ul[@id='nav']/li[2]/ul/li[3]/ul/li[2]/a")
+	@FindBy(xpath = "//ul[@id='nav']/li[2]/ul/li[3]/ul/li[2]/a")
 	WebElement SubSubItem2;
-	
+
+	public WebElement SubSubItem1() {
+		return SubSubItem1;
+	}
+
+	public WebElement SubSubItem2() {
+		return SubSubItem2;
+	}
 
 }
